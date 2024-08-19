9794ef94b414916ded7078d037ede22694427ef8
ThanhTan <tranthanhtantg2003@gmail.com>
2024-08-19 13:52:27 +0700

    tạo cấu trúc dự án

---
+++ analytical-and-reporting-service/src/main/java/com/programmingtechie/analytical_and_reporting_service/AnalyticalAndReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.programmingtechie.analytical_and_reporting_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticalAndReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticalAndReportingServiceApplication.class, args);
+	}
+
+}
---
+++ analytical-and-reporting-service/src/test/java/com/programmingtechie/analytical_and_reporting_service/AnalyticalAndReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingtechie.analytical_and_reporting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticalAndReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
