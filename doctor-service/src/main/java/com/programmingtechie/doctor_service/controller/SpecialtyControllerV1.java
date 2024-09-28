d52b80ef33a2ad9d49cd0bcde53b12671a5fc989
ThanhTan <tranthanhtantg2003@gmail.com>
2024-09-28 11:19:33 +0700

    Cau hinh trien khai cac API co ban cho Doctor phan Specialty

--- doctor-service/src/main/java/com/programmingtechie/doctor_service/controller/SpecialtyControllerV1.java
+++ doctor-service/src/main/java/com/programmingtechie/doctor_service/controller/SpecialtyControllerV1.java
@@ -1,10 +1,14 @@
 package com.programmingtechie.doctor_service.controller;
 
+import com.programmingtechie.doctor_service.dto.response.PageResponse;
+import com.programmingtechie.doctor_service.dto.response.SpecialtyResponse;
 import com.programmingtechie.doctor_service.service.SpecialtyServiceV1;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/doctor/specialty")
@@ -13,4 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpecialtyControllerV1
 {
     final SpecialtyServiceV1 specialtyServiceV1;
+
+    // Lấy tất cả specialties với phân trang
+    @GetMapping("/get-all")
+    public ResponseEntity<PageResponse<SpecialtyResponse>> getAllSpecialties(
+            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+        return ResponseEntity.ok(specialtyServiceV1.getAllSpecialties(page, size));
+    }
+
+    // Lấy specialty theo id
+    @GetMapping("/id/{id}")
+    public ResponseEntity<SpecialtyResponse> getSpecialtyById(@PathVariable String id) {
+        Optional<SpecialtyResponse> specialty = specialtyServiceV1.getSpecialtyById(id);
+        return specialty.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
