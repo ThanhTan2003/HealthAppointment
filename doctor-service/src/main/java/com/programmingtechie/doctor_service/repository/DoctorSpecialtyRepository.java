1eb8b4d8f6b2c5f34b9844f5f3cf86be152bda14
TGT <thinh120203@gmail.com>
2024-10-18 20:47:45 +0700

    add code api gateway

--- doctor-service/src/main/java/com/programmingtechie/doctor_service/repository/DoctorSpecialtyRepository.java
+++ doctor-service/src/main/java/com/programmingtechie/doctor_service/repository/DoctorSpecialtyRepository.java
@@ -1,7 +1,9 @@
 package com.programmingtechie.doctor_service.repository;
 
 import com.programmingtechie.doctor_service.model.DoctorSpecialty;
+import com.programmingtechie.doctor_service.model.DoctorSpecialtyId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DoctorSpecialtyRepository extends JpaRepository<DoctorSpecialty, String> {
+public interface DoctorSpecialtyRepository extends JpaRepository<DoctorSpecialty,DoctorSpecialtyId> {
 }
